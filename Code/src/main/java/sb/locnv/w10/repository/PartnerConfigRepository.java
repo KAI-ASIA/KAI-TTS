75edf8f9cfbda16e7ddbeb8a2ca3d3d60c68de6f
vietloc2901 <nvloc29012000@gmail.com>
2024-12-10 22:58:49 +0700

    ud

--- Code/src/main/java/sb/locnv/w10/repository/PartnerConfigRepository.java
+++
@@ -1,7 +0,0 @@
-package sb.locnv.w10.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sb.locnv.w10.entity.PartnerConfig;
-
-public interface PartnerConfigRepository extends JpaRepository<PartnerConfig, String> {
-}
